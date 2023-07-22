b6f7aeb6876fcbb7f781f2587199d9dc253cc150
Ammad994 <ammad.sarwar5050@gmail.com>
2023-07-22 05:06:40 +0100

    Added 2nd player to play with and added the number of times we play after that game ends

--- Dice_roll_game.py
+++ Dice_roll_game.py
@@ -3,84 +3,88 @@ import random
 
 class DiceRollingGame:
     def __init__(self):
-        self.player_name = ""
-        self.score = 0
-        self.highest_score = 0
+        self.players = []
+        self.num_players = 2  # Number of players
         self.num_rolls = 0
-        self.num_sides = 6  # Default number of sides on the dice
+        self.total_rounds = 0
         self.target_rolls = 10  # Default number of rolls before the game ends
+        self.num_sides = 6  # Default number of sides on the dice
 
-    def get_player_name(self):
-        # Get the player's name as input
-        self.player_name = input("Enter your name: ")
-
-    def get_num_sides(self):
-        # Get the number of sides on the dice as input
-        while True:
-            try:
-                self.num_sides = int(input("Enter the number of sides on the dice (minimum 2): "))
-                if self.num_sides < 2:
-                    raise ValueError
-                break
-            except ValueError:
-                print("Invalid input. Please enter a valid number of sides.")
-
-    def get_target_rolls(self):
-        # Get the number of rolls before the game ends as input
-        while True:
-            try:
-                self.target_rolls = int(input("Enter the number of rolls before the game ends (minimum 1): "))
-                if self.target_rolls < 1:
-                    raise ValueError
-                break
-            except ValueError:
-                print("Invalid input. Please enter a valid number of rolls.")
+        # Get the names of the players
+        for i in range(self.num_players):
+            player_name = input(f"Enter name of Player {i + 1}: ")
+            player_info = {
+                "name": player_name,
+                "score": 0,
+                "highest_score": 0
+            }
+            self.players.append(player_info)
 
     def roll_dice(self):
         # Roll the dice and return the result
         return random.randint(1, self.num_sides)
 
+    def get_target_rolls(self):
+        # Get the number of target rolls from the user
+        while True:
+            try:
+                self.target_rolls = int(input("Enter the number of rolls per player: "))
+                if self.target_rolls <= 0:
+                    print("Please enter a positive number.")
+                else:
+                    break
+            except ValueError:
+                print("Invalid input. Please enter a valid number.")
+
     def play(self):
         # Start the game
-        print(f"Welcome, {self.player_name}! Let's play the Dice Rolling Game.")
-        print(f"You have {self.target_rolls} rolls to achieve the highest score!")
+        print("Welcome to the Dice Rolling Game!")
+        self.get_target_rolls()
+        self.num_rolls = 0
 
-        while self.num_rolls < self.target_rolls:
-            play_again = input("Press Enter to roll the dice or type 'exit' to quit: ")
+        while self.total_rounds < self.target_rolls:
+            for player in self.players:
+                player_name = player["name"]
+                player_score = player["score"]
+                player_highest_score = player["highest_score"]
 
-            if play_again.lower() == 'exit':
-                # Player decides to quit the game
-                print(f"Thanks for playing, {self.player_name}!")
-                print(f"You rolled the dice {self.num_rolls} times.")
-                print("Your final score:", self.score)
-                print("Your highest score:", self.highest_score)
-                break
+                play_again = input(f"{player_name}, press Enter to roll the dice or type 'exit' to quit: ")
 
-            roll_result = self.roll_dice()
-            print(f"You rolled a {roll_result}!")
-            self.num_rolls += 1
+                if play_again.lower() == 'exit':
+                    # Player decides to quit the game
+                    print(f"Thanks for playing, {player_name}!")
+                    print(f"You played a total of {self.total_rounds} rounds.")
+                    print(f"Your final score: {player_score}")
+                    print(f"Your highest score: {player_highest_score}")
+                    break
 
-            if roll_result == self.num_sides:
-                # Player rolled the highest possible number
-                print("Congratulations! You got the highest score!")
-                self.score += self.num_sides
+                roll_result = self.roll_dice()
+                print(f"{player_name}, you rolled a {roll_result}!")
+                self.num_rolls += 1
 
-                if self.score > self.highest_score:
-                    self.highest_score = self.score
+                if roll_result == self.num_sides:
+                    # Player rolled the highest possible number
+                    print("Congratulations! You got the highest score this round!")
+                    player_score += self.num_sides
+
+                    if player_score > player_highest_score:
+                        player["highest_score"] = player_score
+                else:
+                    # Player did not roll the highest possible number
+                    print("Try again to get a higher score this round.")
+
             else:
-                # Player did not roll the highest possible number
-                print("Try again to get a higher score.")
+                # All players have completed their rolls for this round
+                self.total_rounds += 1
+                print(f"Round {self.total_rounds} over!")
+                self.num_rolls = 0
 
-        else:
-            # Player has reached the maximum number of rolls
-            print("Game over! You have reached the maximum number of rolls.")
+        # Game ends
+        print("Game over!")
 
 def main():
     # Create an instance of the DiceRollingGame class and start the game
     game = DiceRollingGame()
-    game.get_player_name()
-    game.get_num_sides()
-    game.get_target_rolls()
     game.play()
 
 if __name__ == "__main__":
