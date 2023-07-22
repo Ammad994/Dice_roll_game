1a94c681431b2486bb1652c0f20f8922cbc0767b
Ammad994 <ammad.sarwar5050@gmail.com>
2023-07-22 03:59:43 +0100

    Added a functionality to allow the player to choose the number of sides on the dice before rolling

--- Dice_roll_game.py
+++ Dice_roll_game.py
@@ -6,14 +6,26 @@ class DiceRollingGame:
         self.player_name = ""
         self.score = 0
         self.num_rolls = 0
+        self.num_sides = 6  # Default number of sides on the dice
 
     def get_player_name(self):
         # Get the player's name as input
         self.player_name = input("Enter your name: ")
 
+    def get_num_sides(self):
+        # Get the number of sides on the dice as input
+        while True:
+            try:
+                self.num_sides = int(input("Enter the number of sides on the dice (minimum 2): "))
+                if self.num_sides < 2:
+                    raise ValueError
+                break
+            except ValueError:
+                print("Invalid input. Please enter a valid number of sides.")
+
     def roll_dice(self):
         # Roll the dice and return the result
-        return random.randint(1, 6)
+        return random.randint(1, self.num_sides)
 
     def play(self):
         # Start the game
@@ -34,18 +46,19 @@ class DiceRollingGame:
             print(f"You rolled a {roll_result}!")
             self.num_rolls += 1
 
-            if roll_result == 6:
-                # Player rolled a 6 (highest score)
+            if roll_result == self.num_sides:
+                # Player rolled the highest possible number
                 print("Congratulations! You got the highest score!")
-                self.score += 6
+                self.score += self.num_sides
             else:
-                # Player did not roll a 6
+                # Player did not roll the highest possible number
                 print("Try again to get a higher score.")
 
 def main():
     # Create an instance of the DiceRollingGame class and start the game
     game = DiceRollingGame()
     game.get_player_name()
+    game.get_num_sides()
     game.play()
 
 if __name__ == "__main__":
